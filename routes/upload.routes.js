5bfd6b0232087cb5699a6453f9b7005d158cd186
Valentina <invalofu.15@gmail.com>
2023-11-29 12:08:49 +0100

    done

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,6 +1,17 @@
 
-const router = require("express")
+const router = require("express").Router()
 
+// const uploader = require('./../middleware/cloudinary.middleware')
+
+// router.post('/image', uploader.single('imageData'), (req, res) => {
+
+//     if (!req.file) {
+//         res.status(500).json({ errorMessage: 'Error caragndo el archivo' })
+//         return
+//     }
+
+//     res.json({ cloudinary_url: req.file.path })
+// })
 
 
 module.exports = router
