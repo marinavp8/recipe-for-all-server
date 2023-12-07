b205de6ce83316303905392fab57126c73a72da1
Valentina <invalofu.15@gmail.com>
2023-12-07 14:55:30 +0100

    Almost done ...

--- config/index.js
+++ config/index.js
@@ -20,13 +20,13 @@ module.exports = (app) => {
   app.use(express.urlencoded({ extended: false }))
   app.use(cookieParser())
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*')
 
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  //   res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 
-    next()
-  })
+  //   next()
+  // })
 }
